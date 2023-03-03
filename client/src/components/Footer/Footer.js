cab22af02ae7552c0e8847932534a957fc452890
mshoaibtalha <mshoaibtalha@gmail.com>
2023-03-03 17:18:23 +0100

    footer done

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,5 +1,18 @@
 import React from 'react';
+import './Footer.scss';
 
 export const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className='footer'>
+      <div className='bottom'>
+        <div className='left'>
+          <span className='logo'>Shoaibs Eshop</span>
+          <span className='copyright'>@ Copy 2023. All Rights Reserved</span>
+        </div>
+        <div className='right'>
+          <img src='/img/stripe.png' alt='' />
+        </div>
+      </div>
+    </div>
+  );
 };
