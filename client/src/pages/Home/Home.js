8a65d72c212c8e47cbfc71eee4b762ab9ea2ac5d
mshoaibtalha <mshoaibtalha@gmail.com>
2023-03-09 16:37:36 +0100

    contact page completed

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -3,6 +3,7 @@ import Slider from '../../components/slider/Slider';
 import './Home.scss';
 import FeaturedProducts from '../../components/FeaturedProduct/FeaturedProducts';
 import Categories from '../../components/catagory/Categories';
+import Contact from '../../components/contact/Contact';
 
 export const Home = () => {
   return (
@@ -11,6 +12,7 @@ export const Home = () => {
       <FeaturedProducts type='Featured' />
       <Categories />
       <FeaturedProducts type='Trending' />
+      <Contact />
     </div>
   );
 };
