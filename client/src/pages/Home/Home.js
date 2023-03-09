a4fb676c1e430377bf7061691adb5202d953b704
mshoaibtalha <mshoaibtalha@gmail.com>
2023-03-09 16:04:09 +0100

    categories page finished

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Slider from '../../components/slider/Slider';
 import './Home.scss';
 import FeaturedProducts from '../../components/FeaturedProduct/FeaturedProducts';
+import Categories from '../../components/catagory/Categories';
 
 export const Home = () => {
   return (
     <div>
       <Slider />
       <FeaturedProducts type='Featured' />
+      <Categories />
       <FeaturedProducts type='Trending' />
     </div>
   );
